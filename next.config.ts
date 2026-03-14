84bc8fefbba689fccb60ead0a48810033dbe9bb2
varunkarankar32 <iec2023037@iiita.ac.in>
2026-03-15 00:41:01 +0530

    chore: point api routes to hugginface space backend

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: "https://varunka-janshakti-backend.hf.space/api/:path*",
       },
     ];
   },
